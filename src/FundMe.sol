329e86c6169a1ed621d266d1267741c738824872
worksofallwin <princeallwin199@gmail.com>
2024-01-05 17:19:31 +0530

    Refactor fundMe code

---
+++ src/FundMe.sol
@@ -0,0 +1,160 @@
+// Todo
+// Get funds from users
+// Allow owner to withdraw funds
+// Set a minimum funding value in USD
+
+// Note
+// similar to wallets, Smart Contracts can hold funds as well
+// To receive funds, we have to make the function payable
+
+// 1 ETH = 1e18 = 1000000000000000000 WEI = 1 * 10 ** 18
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import {PriceConverter} from "./PriceConverter.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+// cutom errors
+error FundMe__NOT_OWNER();
+error FundMe__WITHDRAW_FAILED();
+error FundMe__NOT_ENOUGH_ETH();
+
+contract FundMe {
+    AggregatorV3Interface public i_priceFeed;
+    // price feed address for ETH/USD
+
+    address public immutable i_owner;
+
+    constructor(address _priceFeed) {
+        i_priceFeed = AggregatorV3Interface(_priceFeed);
+        i_owner = msg.sender;
+    }
+    // when the contract gets deployed
+    // constructor will be automatically called and executed
+
+    // by using "constant" and "immutable" we are able to save gas
+    // Reason for that is instead of storing these variables into storage slots
+    // We actually store them directly into the byte code of the contract
+
+    using PriceConverter for uint256;
+    // for all uint256 we can use the PriceConverter library
+
+    uint256 public constant MINIMUM_USD = 5e18;
+    // since priceInUsd will have 18 decimals, minimum USD should also have 18 decimals
+
+    address[] public s_funders;
+    // whenver someone funds this contract, their address will be stored in this array to keep trace of all the funders
+    // using array, we can keep track of all the funders, but we cannot keep track of the amount each funder funded
+    // for that we have to use mapping
+    // since funders is a storage variable we use s_funders
+
+    mapping(address funder => uint256 amountFunded) public s_addressToAmountFunded;
+
+    // Anyone can call fund() so we have to make it as public
+    // Todo
+    // Allow users to send money
+    // Set a minimum amount of $5
+
+    // 1. How do we send ETH to this contract?
+    // Whenever we send a transaction on the blockchain there is a "VALUE" field populated
+    // most of the time it is sent with 0 WEI if no ether is sent
+
+    // To make a function receive ether we have to make the function "PAYABLE"
+    // To access the funds sent to the contract we can use "msg.value"
+
+    // Revert -> undo any actions that have been done, and send the remaining gas back
+    // If a tx reverts in the middle of a process, any changes done to the blockchain will be reverted
+    // However let's say user sent 2100 gas to make a tx and before reverting 1000 gas has been used
+    // Remaining 1100 gas will be reverted to the user.
+
+    function fund() public payable {
+        if (msg.value.getConversionRate(i_priceFeed) < MINIMUM_USD) {
+            revert FundMe__NOT_ENOUGH_ETH();
+        }
+        // since we are using PriceConverter library
+        // uint256 is the first input variable type to getConversionRate()
+        // when we are using library, the first input variable is the type we are using with the library
+        // msg.value is of type uint256
+        // therfore msg.value is passed inside the getConversionRate() as first variable
+
+        s_funders.push(msg.sender);
+        // msg.sender will hold the address of the sender
+
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        // If a address is sending funds for the 2nd time
+        // this amount should be added with the previous
+    }
+
+    // this is not the efficient way to withdraw
+    // refer Foundry_FundMe
+    function withdraw() public onlyOwner {
+        address[] memory exstingFunders = s_funders;
+        for (uint256 funderIndex = 0; funderIndex < exstingFunders.length; funderIndex++) {
+            address funder = exstingFunders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        // resetting all the amounts to 0
+
+        s_funders = new address[](0);
+        // since all the amount funded got withdrawn
+        // we have to reset the funders[]
+        // (0) -> start at 0.
+
+        (bool sent,) = payable(i_owner).call{value: address(this).balance}("");
+
+        // To send ETH from this contract to EOA or other contracts
+        // use the recommended call method
+        // call (forward all gas or set gas, returns bool)
+        // to send ether, receiving address has to be marked as type payable
+
+        // "this" refers to the current contract
+
+        if (!sent) {
+            revert FundMe__WITHDRAW_FAILED();
+        }
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert FundMe__NOT_OWNER();
+        }
+        _;
+
+        // If we didn't use custom errors
+        // Then we are storing this string as string array in memory
+        // For example, a string "Hello" would be stored as an array of bytes [72, 101, 108, 108, 111],
+        // where each byte represents the ASCII value of the corresponding character.
+
+        // By using revert, we can return the error code instead of string
+        // using custom erros is gas efficient
+    }
+
+    // Function to receive Ether. when msg.data must be empty
+    receive() external payable {
+        fund();
+    }
+    // receiver() must have external visibility and payable state mutability.
+
+    // Fallback function is called when msg.data is not empty
+    // when we send some data, it will check whether it matches with any of the functions defined.
+    // If none of the function is matched, it will look for the fallback()
+    fallback() external payable {
+        fund();
+    }
+    // fallback() must have external visibility and payable state mutability.
+}
+
+/*
+           send Ether
+               |
+         msg.data is empty?
+              / \
+            yes  no
+            /     \
+receive() exists?  fallback()
+         /   \
+        yes   no
+        /      \
+    receive()   fallback()
+*/
---
+++ src/PriceConverter.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+// library is similar to a contract
+//  but you can't declare any state variable and you can't send ether.
+// A library is embedded into the contract if all library functions are internal.
+
+library PriceConverter {
+    // get the price for ETH/USD
+
+    function getPrice(AggregatorV3Interface _priceFeed) internal view returns (uint256) {
+        // To find the price of ETH/USD
+        // we need that contact address and
+        // ABI -> ABI exposes all the functions available in that contract
+        // using this ABI, external contracts can interact with that contract
+        // 0x694AA1769357215DE4FAC081bf1f309aDC325306
+
+        (, int256 price,,,) = _priceFeed.latestRoundData();
+
+        // Note
+        // this price will give the Price of ETH in terms of USD
+        // price will have 8 deciaml places
+        // msg.value will be in terms of WEI which means 18 decimal places
+        // to matchup price and msg.value we have to convert price to 18 deciamls
+        // price has already 8 decimal places we have to add another 10 decimals
+        // price * 1e10 will convert price into 18 decimals
+        // price is int and msg.value is uint
+        // let's convert everything into uint256
+
+        return uint256(price * 1e10);
+
+        // Now this getPrice() will return price of 1 ETH in terms of USD
+    }
+
+    // get the conversion rate
+    // If msg.value = 0.1 ETH then what is the value in USD?
+
+    function getConversionRate(uint256 ethAmount, AggregatorV3Interface _priceFeed) internal view returns (uint256) {
+        // this getConversionRate will get msg.value and ethPrice can be obtained from getPrice()
+
+        // step1
+        // get the price of ETH using getPrice()
+        uint256 ethPrice = getPrice(_priceFeed);
+
+        // if msg.value is 1 ETH then 1e18*return val of getPrice()
+        // for eg return value of getPrice() is 2000e18
+        // then 1e18 * 2000e18 = 2000e36
+        // since we need only 18 decimals we have to divide it by 18
+        // 2000e36/1e18 = 2000e18
+        // if msg.value is 0.5 ETH
+        // then 0.5e18 * 2000e18 = 1000e36
+        // 1000e36/1e18 = 1000e18
+
+        uint256 ethAmountInUsd = (ethAmount * ethPrice) / 1e18;
+
+        // let's say ethAmount is 0.5e18
+        // ethPrice = 2000e18
+        // ethAmountInUsd = (0.5e18 * 2000e18)/1e18
+        // ethAmountInusd = 1000e18
+
+        return ethAmountInUsd;
+    }
+}
